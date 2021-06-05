8a8c9b3da58528670ce1353e3fd386b084bb003f
Jeremy Perron <jeremy.perron@us.sogeti.com>
2021-06-05 10:39:10 -0500

    refactor config into its own file

---
+++ config.js
@@ -0,0 +1,12 @@
+// Global Config for channel setup
+var globalConfig = {
+    clientid: '<clientId>',
+    clientSec: '<clientSec>',
+    channelName: '<ChannelName>'
+}
+
+// Shoutout Clip specific config
+var shoutoutConfig = {
+    showTitle: true,
+    title: '<Shoutout Title Text>'
+}
