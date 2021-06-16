5107866d8236b6a9c1acd7001e9d7ab7a9024ade
OELSHAMI_TTV <82464333+OELSHAMI-ttv@users.noreply.github.com>
2021-06-15 21:17:47 -0400

    Code to play a Sound for users

---
+++ soundconfig.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+// Global Config for channel setup
+var globalConfig = {
+    clientid: '<clientId>',
+    clientSec: '<clientSec>',
+    channelName: '<ChannelName>'
+}
+
+const boardCAST = "ilouminati";
+const GenericConst = 60;
+var Generic = 0;
+var GenSnd = 'obiwanhellothere.mp3';
+var MaxMe = 5;
+var Silence = 0;
+
+//User Sound List 
+var clipsArray = [
+    { name: 'ilouminati', file: 'pornhub-community-intro.mp3', run: 0 }
+    ,{ name: 'oelshami', file: 'airwolf.mp3', run: 0 }
+    //Format to add more
+    //,{ name: 'twitchname', file: 'soundfile', run: 0 }
+];
