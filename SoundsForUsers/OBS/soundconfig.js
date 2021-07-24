b428e166a3375141053e2db4949f807709b8d251
OELSHAMI_TTV <82464333+OELSHAMI-ttv@users.noreply.github.com>
2021-07-24 15:05:05 -0400

    Update

--- SoundsForUsers/OBS/soundconfig.js
+++ SoundsForUsers/OBS/soundconfig.js
@@ -14,9 +14,14 @@ const GenericConst = 60;
 var Generic = 0;
 //Sound can be wav/mp3/ogg (as tested, more may be supported)
 var GenSnd = '<GenericSoundGreeting.mp3>';
+var GenSndOn = true;
 var MaxMe = 5;
 var Silence = 0;
 var cmdMe = "me"
+var silent = 'silence'
+var quiet = 'quiet'
+var wakeup = 'wakeup'
+var CoolDown = 15000
 
 //Welcome User for String is @{user}
 
@@ -30,3 +35,13 @@ var clipsArray = [
     //Format to add more
     //,{ name: 'twitchname', file: 'soundfile', run: 0 }
 ];
+
+var cmdArray = [
+    { command: '<enter cmdname>', file: '<SoundFileName>', flag: '<Enter Users Flags>', user: '<twitch user>' }
+   ,{ command: '<enter cmdname>', file: '<SoundFileName>', flag: '<Enter Users Flags>', user: '<twitch user>'}
+   //Flags:
+   // broadcaster, mod, vip, sub, all or Twitch User
+   // broadcaster: broadcaster, mod: mod, vip: vip, sub : sub user:twitchName, all : all
+   //Can combine broadcastermodvipsuball or anywhich way.
+   //Twitch User can be user name oelshamikingkira66 but if no user is required leave '<twitch user>'
+];  
